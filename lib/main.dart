e1061bf2bbab354b5f0c40312a7ceeae9fcf9616
mariell00 <doydora.mariel02@gmail.com>
2025-03-12 13:00:36 +0800

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
-import 'login_page.dart'; 
+import 'package:firebase_core/firebase_core.dart'; // Import Firebase core
+import 'login_page.dart'; // Import the login page
 
-void main() {
-  runApp(SmartBiteApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Ensure binding is initialized
+  await Firebase.initializeApp(); // Initialize Firebase
+
+  runApp(const MyApp());
 }
 
-class SmartBiteApp extends StatelessWidget {
-  const SmartBiteApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(), 
+      title: 'AppDev',
+      home: LoginPage(),
+    );
   }
 }
